f22d73b00e9b110d535a3268f00af18df859e877
Akhil <akhilkb543@gmail.com>
2025-03-24 14:38:13 +0530

    Matrix operations

--- oops lab/matrixoperations.java
+++ oops lab/matrixoperations.java
@@ -29,8 +29,12 @@ public class matrixoperations {
     }
 
     public static void main(String[] args) {
-        int r1,r2,c1,c2;
+        int r1,r2,c1,c2,c=1;
         Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("Matrix operations:");
+        } while (c<4);
 
         //input for matrix 1
         System.out.println("Enter the number of rows of matrix 1: ");
@@ -48,20 +52,27 @@ public class matrixoperations {
         matrixoperations m2 = new matrixoperations(r2,c2);
         m2.input();
 
-        matrixoperations m3=new matrixoperations(r1,c1);
-        for(int i=0;i<r1;i++){
-            for(int j=0;j<c1;j++){
-                m3.matrix[i][j] = m1.matrix[i][j] + m2.matrix[i][j];
-            }
-        }
-        m3.display();
+        // matrixoperations m3=new matrixoperations(r1,c1);
+        // for(int i=0;i<r1;i++){
+        //     for(int j=0;j<c1;j++){
+        //         m3.matrix[i][j] = m1.matrix[i][j] + m2.matrix[i][j];
+        //     }
+        // }
+        // m3.display();
 
         matrixoperations m4=new matrixoperations(r1, c2);
-        int sum=0;
+        
         for(int i=0;i<r1;i++){
             for(int j=0;j<c2;j++){
-                m4.matrix[i][j] = 0;
+                int sum=0;
+                for(int k=0;k<c1;k++){
+                    sum=sum+m1.matrix[i][k]*m2.matrix[k][j];
+                }
+                m4.matrix[i][j]=sum;
             }
         }
+
+        m4.display();
+
     }
 }
